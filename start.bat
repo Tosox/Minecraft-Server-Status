4df8fabcb95b57713759b89cd054d82fe26a7de7
Dennis <benjamingriller@gmail.com>
2022-12-24 11:14:18 +0100

    fix: empty port input

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 title Minecraft Server Status
 set /p address="Minecraft server address: "
 set /p port="Server port (leave empty to use default port): "
-if "%port%"==[] set port="25565"
+if not defined port set port="25565"
 python ./status_check.py -ip %address% --port %port%
 pause
